b4e1aae82eb60a460309f7a7575e29d1919bdb3c
gosavisomnath <somnathgosavi29.sg@gmail.com>
2021-08-31 11:40:10 +0530

    Contact Added

--- AddressBook.js
+++ AddressBook.js
@@ -1,1 +1,30 @@
-console.log("Welocme to Address Book Problem Using Java Script");
+class Contact
+{
+    //properties
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phoneNo;
+    email;
+
+    //Creatinng a method constructor 
+    constructor(...params)
+    {
+        this.firstName = params[0];
+        this.lastName = params[1];
+        this.address = params[2];
+        this.city = params[3];
+        this.state = params[4];
+        this.zip = params[5];
+        this.phoneNo = params[6];
+        this.email = params[7];
+    }
+}
+
+//Creating object
+let contact1 = new Contact("soma", "Gosavi", "Sus", "Pune", "Maharashtra",411028,123456789, "soma@gmail.com")
+//calling the onject here
+console.log(contact1)
